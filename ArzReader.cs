dcb8943e4676e63c2ffd0b0e84acc2c64294b837
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2022-11-13 19:14:36 +0100

    impr(ArzReader): refactoring fix reading too many infos if reading multiple times

--- ArzReader.cs
+++ ArzReader.cs
@@ -74,6 +74,8 @@ namespace TQArchive_Wrapper
                 {
                     yield return stringListEnumerator.Current;
                 }
+                if (stringList.Count == numStrings)
+                    yield break;
             }
 
             using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
@@ -149,10 +151,13 @@ namespace TQArchive_Wrapper
                 ReadHeader(reader);
 
             stream.Seek(lastStringOffset, SeekOrigin.Begin);
+
             if (numStrings == -1)
                 numStrings = reader.ReadInt32();
+
             CheckStringID(id);
             var preloadMax = Math.Min(numStrings, maxID + 6);
+
             for (int i = stringList.Count; i < preloadMax; i++)
             {
                 var currStr = string.Empty;
@@ -173,10 +178,12 @@ namespace TQArchive_Wrapper
             lastStringOffset = stream.Position;
         }
 
+        //public string GetString(int index) => GetStrings(index).First();
+
         public string GetString(int index)
         {
             CheckStringID(index);
-            while (stringList.Count > index)
+            if (stringList.Count > index)
             {
                 return stringList[index];
             }
@@ -187,36 +194,41 @@ namespace TQArchive_Wrapper
                 ReadHeader(reader);
 
             stream.Seek(lastStringOffset, SeekOrigin.Begin);
+
             if (numStrings == -1)
                 numStrings = reader.ReadInt32();
+
             CheckStringID(index);
             string ret = string.Empty;
             var preloadMax = Math.Min(numStrings, index + 6);
+
             for (int i = stringList.Count; i < preloadMax; i++)
             {
                 var currStr = string.Empty;
 
                 currStr = reader.ReadCString();
                 stringList.Add(currStr);
-                lastStringOffset = stream.Position;
 
                 if (i == index)
                 {
                     ret = currStr;
                 }
             }
+            lastStringOffset = stream.Position;
             return ret;
         }
 
         public IEnumerable<DBRFileInfo> GetDBRFileInfos()
         {
-            if (fileInfos.Count > 0)
+            if (headerInitialised && fileInfos.Count > 0)
             {
                 var dbrFileInfoEnumerator = fileInfos.GetEnumerator();
                 while (dbrFileInfoEnumerator.MoveNext())
                 {
                     yield return dbrFileInfoEnumerator.Current;
                 }
+                if (fileInfos.Count == header.DBNumEntries)
+                    yield break;
             }
 
             using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
@@ -226,14 +238,14 @@ namespace TQArchive_Wrapper
 
             stream.Seek(lastFileInfoOffset, SeekOrigin.Begin);
 
-            for (int i = 0; i < header.DBNumEntries; i++)
+            for (int i = fileInfos.Count; i < header.DBNumEntries; i++)
             {
                 var currInfo = DBRFileInfo.ReadFrom(reader);
-                lastFileInfoOffset = stream.Position;
                 fileInfos.Add(currInfo);
                 //mappedFileInfos.Add(GetString(currInfo.NameID), currInfo);
                 yield return currInfo;
             }
+            lastFileInfoOffset = stream.Position;
         }
 
         public IEnumerable<RawDBRFile> GetFiles()
@@ -259,8 +271,8 @@ namespace TQArchive_Wrapper
         private RawDBRFile ReadDBRFile(Stream stream, DBRFileInfo info)
         {
             var fileName = GetString(info.NameID);
-
             var fileOffset = info.Offset + ArzHeader.DBTableBaseOffset;
+
             if (mappedFileInfos.TryGetValue(fileName, out var cachedInfo))
                 if (info.Offset != cachedInfo.Offset)
                 {
@@ -269,152 +281,164 @@ namespace TQArchive_Wrapper
 
             if (files.TryGetValue(fileName, out var cachedFile))
                 return cachedFile;
+
             try
             {
-                // Copy compressed file to buffer used in MemoryStream
-                stream.Seek(fileOffset, SeekOrigin.Begin);
-                var buffer = new byte[info.CompressedLength];
-                var bytesRead = stream.Read(buffer);
-                stream.Dispose();
-                if (bytesRead != info.CompressedLength)
-                {
-                    var exc = new Exception("Error reading dbr compressed file");
-                    logger?.LogError(exc, "Error reading file at {offset}, reading out of bounds", fileOffset);
-                    throw exc;
-                }
-                using var inflatedStream = new MemoryStream(buffer);
+                using MemoryStream inflatedStream = ExtractCompressedFile(stream, fileOffset, info.CompressedLength);
 
-                // Deflate/Decompress file into new buffer used in MemoryStream
-                using var deflater = new ZLibStream(inflatedStream, CompressionMode.Decompress, true);
+                using MemoryStream deflatedStream = CreateDecompressedStream(inflatedStream);
 
-                var estimate = 1024;
-                using var deflatedStream = new MemoryStream(estimate);
-                var deflatedBuffer = new byte[estimate];
-                int len;
-                while ((len = deflater.Read(deflatedBuffer)) > 0)
-                {
-                    deflatedStream.Write(deflatedBuffer, 0, len);
-                }
-                deflater.Dispose();
-                inflatedStream.Dispose();
-
-                // Reset MemoryStream and create reader for decompressed data
-                deflatedStream.Position = 0;
-                using var reader = new BinaryReader(deflatedStream);
-                var rawEntries = new Dictionary<string, string>();
-
-                // Read all decompressed data
-                while (reader.PeekChar() >= 0)
-                {
-                    // Read variable info
-                    var type = reader.ReadInt16();
-                    var numValues = reader.ReadInt16();
-                    var nameID = reader.ReadInt32();
-                    var varName = GetString(nameID);
-
-                    // Check Variable validity
-                    if (numValues < 1)
-                    {
-                        logger?.LogWarning("Found variable {name} with less than 1 value, skipping!", varName);
-                        continue;
-                    }
-                    string typeStr = string.Empty;
-                    switch (type)
-                    {
-                        case 0:
-                            typeStr = "int";
-                            break;
-                        case 1:
-                            typeStr = "real";
-                            break;
-                        case 2:
-                            typeStr = "string";
-                            break;
-                        case 3:
-                            typeStr = "bool";
-                            break;
-                        default:
-                            var exc = new Exception("Error reading dbr compressed file");
-                            logger?.LogError(exc, "Found variable {name} of invalid type {type}, skipping!", varName, type);
-                            // May lead to reading unknown stuff
-                            throw exc;
-                    }
-
-                    StringBuilder builder = new();
-                    // Read variable value based on type
-                    switch (type)
-                    {
-                        case 0:
-                        case 3:
-                            builder.Append(reader.ReadInt32().ToTQString());
-
-                            for (int v = 1; v < numValues; v++)
-                            {
-                                builder.Append(';');
-                                builder.Append(reader.ReadInt32().ToTQString());
-                            }
-                            break;
-                        case 1:
-                            builder.Append(reader.ReadSingle().ToTQString());
-
-                            for (int v = 1; v < numValues; v++)
-                            {
-                                builder.Append(';');
-                                builder.Append(reader.ReadSingle().ToTQString());
-                            }
-                            break;
-                        case 2:
-                            // strings are written as ints referencing indices in the string table
-                            var stringIDs = new int[numValues];
-                            for (int v = 0; v < numValues; v++)
-                            {
-                                stringIDs[v] = reader.ReadInt32();
-                            }
-
-                            var stringValues = GetStrings(stringIDs);
-                            if (numValues > 1)
-                                builder.Append(string.Join(";", stringValues));
-                            else
-                                builder.Append(stringValues.First());
-
-                            break;
-                    }
-                    var value = builder.ToString();
-
-                    rawEntries.Add(varName, value);
-                }
+                var rawEntries = ReadRawEntries(deflatedStream);
                 if (!rawEntries.TryGetValue(TQDB_Parser.Constants.TemplateKey, out var templateName))
                 {
                     var exc = new Exception("Missing templateName");
-                    logger?.LogError(exc, "Error reading file at {offset}", fileOffset);
                     throw exc;
                 }
                 rawEntries.Remove(TQDB_Parser.Constants.TemplateKey);
 
                 return new RawDBRFile { FileName = fileName, TemplateName = templateName, RawEntries = rawEntries };
-                //try
-                //{
-                //    var tplRoot = tplManager.GetRoot(templateName);
-                //    tplRoot.ResolveIncludes(tplManager);
-
-                //    var parser = new DBRParser(tplManager, logger);
-                //    var entries = parser.ParseEntries(fileName, tplRoot, rawEntries);
-
-                //    var file = new DBRFile(fileName, tplRoot, entries, logger);
-                //    files.Add(fileName, file);
-                //    return file;
-                //}
-                //catch (Exception e)
-                //{
-                //    logger?.LogError(e, "Error reading file {name} at {offset}", fileName, fileOffset);
-                //    throw;
-                //}
             }
             catch (Exception e)
             {
                 logger?.LogError(e, "Error reading file at {offset}", fileOffset);
                 throw;
             }
+        }
+
+        private Dictionary<string, string> ReadRawEntries(MemoryStream deflatedStream)
+        {
+            // Create reader for decompressed data
+            using var reader = new BinaryReader(deflatedStream);
+            var rawEntries = new Dictionary<string, string>();
+
+            // Read all decompressed data
+            while (reader.PeekChar() >= 0)
+            {
+                // Read variable info
+                var type = reader.ReadInt16();
+                var numValues = reader.ReadInt16();
+                var nameID = reader.ReadInt32();
+                var varName = GetString(nameID);
+
+                // Check Variable validity
+                if (numValues < 1)
+                {
+                    logger?.LogWarning("Found variable {name} with less than 1 value, skipping!", varName);
+                    // Might lead to reading unknown stuff
+                    continue;
+                }
+                // Read variable value based on type
+                string value;
+                switch (type)
+                {
+                    case 0:
+                        value = ReadIntValue(reader, numValues);
+                        break;
+                    case 1:
+                        value = ReadFloatValue(reader, numValues);
+                        break;
+                    case 2:
+                        value = ReadStringValue(reader, numValues);
+                        break;
+                    case 3:
+                        value = ReadIntValue(reader, numValues);
+                        break;
+                    default:
+                        var exc = new Exception("Error reading dbr compressed file");
+                        logger?.LogError(exc, "Found variable {name} of invalid type {type}, skipping!", varName, type);
+                        // May lead to reading unknown stuff
+                        throw exc;
+                }
+
+                rawEntries.Add(varName, value);
+            }
+            return rawEntries;
+        }
+
+        private string ReadIntValue(BinaryReader reader, int numValues)
+        {
+            StringBuilder builder = new();
+            builder.Append(reader.ReadInt32().ToTQString());
+
+            for (int v = 1; v < numValues; v++)
+            {
+                builder.Append(';');
+                builder.Append(reader.ReadInt32().ToTQString());
+            }
+
+            return builder.ToString();
+        }
+
+        private string ReadFloatValue(BinaryReader reader, int numValues)
+        {
+            StringBuilder builder = new();
+            builder.Append(reader.ReadSingle().ToTQString());
+
+            for (int v = 1; v < numValues; v++)
+            {
+                builder.Append(';');
+                builder.Append(reader.ReadSingle().ToTQString());
+            }
+
+            return builder.ToString();
+        }
+
+        private string ReadStringValue(BinaryReader reader, int numValues)
+        {
+            StringBuilder builder = new();
+            // strings are written as ints referencing indices in the string table
+            var stringIDs = new int[numValues];
+            for (int v = 0; v < numValues; v++)
+            {
+                stringIDs[v] = reader.ReadInt32();
+            }
+
+            var stringValues = GetStrings(stringIDs);
+            //if (numValues > 1)
+            builder.Append(string.Join(";", stringValues));
+            //else
+            //    builder.Append(stringValues.First());
+
+            return builder.ToString();
+        }
+
+        private static MemoryStream CreateDecompressedStream(MemoryStream inflatedStream)
+        {
+            // Deflate/Decompress file into new buffer used in MemoryStream
+            using var deflater = new ZLibStream(inflatedStream, CompressionMode.Decompress, true);
+            var estimate = 1024;
+            var deflatedStream = new MemoryStream(estimate);
+            var deflatedBuffer = new byte[estimate];
+            int len;
+            while ((len = deflater.Read(deflatedBuffer)) > 0)
+            {
+                deflatedStream.Write(deflatedBuffer, 0, len);
+            }
+            deflater.Dispose();
+            inflatedStream.Dispose();
+
+            // Reset MemoryStream
+            deflatedStream.Position = 0;
+            return deflatedStream;
+        }
+
+        private MemoryStream ExtractCompressedFile(Stream stream, int fileOffset, long compressedLength)
+        {
+            // Copy compressed file to buffer used in MemoryStream
+            stream.Seek(fileOffset, SeekOrigin.Begin);
+            var buffer = new byte[compressedLength];
+            var bytesRead = stream.Read(buffer);
+            // risky but efficient
+            stream.Dispose();
+
+            if (bytesRead != compressedLength)
+            {
+                var exc = new Exception("Error reading dbr compressed file");
+                logger?.LogError(exc, "Error reading file at {offset}, reading out of bounds", fileOffset);
+                throw exc;
+            }
+            var inflatedStream = new MemoryStream(buffer);
+            return inflatedStream;
         }
     }
 }
