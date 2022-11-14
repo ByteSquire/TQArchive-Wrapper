6dd3558fb9f6dea3853a89b1d144d97bda101025
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2022-11-14 12:06:09 +0100

    fix(ArzWriter): togglable parallel, don't remove faulty string dumb idea

--- ArzWriter.cs
+++ ArzWriter.cs
@@ -30,103 +30,26 @@ namespace TQArchive_Wrapper
 
         public event Action<string>? FileDone;
 
-        public void Write(IEnumerable<DBRFile> files)
+        public void Write(IEnumerable<DBRFile> files, bool useParallel = false)
         {
             //var strEntries = new List<string>();
             combinedStrings.Clear();
-            var binaryEntries = new List<(DBRFileInfo, MemoryStream)>();
+            IEnumerable<(DBRFileInfo, MemoryStream)> binaryEntries;
 
-            foreach (var file in files)
-            //Parallel.ForEach(files, file =>
+            if (useParallel)
             {
-                using var fileVarsStream = new MemoryStream();
-
-
-                // write templateName as variable, it's excluded in DBRFile entries
-                WriteValue(fileVarsStream, (short)2);
-                WriteValue(fileVarsStream, (short)1);
-
-                int fileNameID;
-                lock (stringDictionaryLock)
-                {
-                    fileNameID = AddStrGetIndex(file.FilePath[file.FilePath.IndexOf("records")..]);
-                    WriteValue(fileVarsStream, AddStrGetIndex("templateName", false));
-                    WriteValue(fileVarsStream, AddStrGetIndex(file.TemplateRoot.FileName));
-                }
-
-                // filter entries, eqnVariables are internal and includes should be resolved by now
-                var entries = file.Entries
-                    .Where(x => x.Template.Type != TQDB_Parser.VariableType.eqnVariable)
-                    .Where(x => x.Template.Type != TQDB_Parser.VariableType.include)
-                    // in a perfect world I'd enable that but a lot of the templates have errors :(
-                    //.Where(x => x.IsValid())
-                    // ignore empty values, maybe whitespace as well?
-                    .Where(x => !string.IsNullOrEmpty(x.Value))
-                    .ToList();
-                // ArtManager orders this way
-                entries = entries.OrderBy(x => x.Name[0]).ThenBy(x => x.Name).ToList();
-
-                foreach (var entry in entries)
-                {
-                    // name of the variable as int index in string table
-                    var stringID = AddStrGetIndexLocked(entry.Name, false);
-
-                    string[] arraySplit = new string[] { entry.Value };
-                    bool isArray = entry.Template.Class == TQDB_Parser.VariableClass.array;
-                    if (isArray)
-                        arraySplit = entry.Value.Split(';');
-                    short numElements = (short)arraySplit.Length;
-
-                    short typeID = -1;
-                    object? values = null;
-                    bool error = false;
-                    // type as int16
-                    switch (entry.Template.Type)
-                    {
-                        case TQDB_Parser.VariableType.@int:
-                            if (!TryGetIntValues(arraySplit, out values))
-                                error = true;
-                            typeID = 0;
-                            break;
-                        case TQDB_Parser.VariableType.real:
-                            typeID = 1;
-                            if (!TryGetFloatValues(arraySplit, out values))
-                                error = true;
-                            break;
-                        case TQDB_Parser.VariableType.file:
-                            typeID = 2;
-                            values = AddStrsGetIndicesAsync(arraySplit, true);
-                            break;
-                        case TQDB_Parser.VariableType.@string:
-                        case TQDB_Parser.VariableType.equation:
-                            typeID = 2;
-                            values = AddStrsGetIndicesAsync(arraySplit, false);
-                            break;
-                        case TQDB_Parser.VariableType.@bool:
-                            typeID = 3;
-                            if (!TryGetIntValues(arraySplit, out values))
-                                error = true;
-                            break;
-                    }
-                    if (error || values is null)
-                    {
-                        combinedStrings.Remove(entry.Name);
-                        continue;
-                    }
-
-                    WriteVariable(fileVarsStream, stringID, typeID, values);
-                }
-                // compress the values using ZLib
-                var myBinaryValuesStream = CompressValues(fileVarsStream);
-
-                // dbr file info
-                DBRFileInfo fileInfo = CreateFileInfo(file, fileNameID, (int)myBinaryValuesStream.Length);
-
-                binaryEntries.Add((fileInfo, myBinaryValuesStream));
-
-                FileDone?.Invoke(file.FilePath);
+                var parallelBinaryEntries = new ConcurrentBag<(DBRFileInfo, MemoryStream)>();
+                binaryEntries = parallelBinaryEntries;
+                Parallel.ForEach(files, file => WriteFile(file, parallelBinaryEntries.Add));
             }
-            //);
+            else
+            {
+                var parallelBinaryEntries = new List<(DBRFileInfo, MemoryStream)>();
+                binaryEntries = parallelBinaryEntries;
+                foreach (var file in files)
+                    WriteFile(file, parallelBinaryEntries.Add);
+            }
+
             using var binaryValuesStream = new MemoryStream();
             using var combinedDBREntries = new MemoryStream();
             foreach ((var info, var stream) in binaryEntries)
@@ -184,6 +107,95 @@ namespace TQArchive_Wrapper
             {
                 logger?.LogError(e, "Error writing to arz archive {archive}", filePath);
             }
+        }
+
+        private void WriteFile(DBRFile file, Action<(DBRFileInfo, MemoryStream)> addToList)
+        {
+            using var fileVarsStream = new MemoryStream();
+
+            // write templateName as variable, it's excluded in DBRFile entries
+            WriteValue(fileVarsStream, (short)2);
+            WriteValue(fileVarsStream, (short)1);
+
+            int fileNameID;
+            lock (stringDictionaryLock)
+            {
+                fileNameID = AddStrGetIndex(file.FilePath[file.FilePath.IndexOf("records")..]);
+                WriteValue(fileVarsStream, AddStrGetIndex("templateName", false));
+                WriteValue(fileVarsStream, AddStrGetIndex(file.TemplateRoot.FileName));
+            }
+
+            // filter entries, eqnVariables are internal and includes should be resolved by now
+            var entries = file.Entries
+                .Where(x => x.Template.Type != TQDB_Parser.VariableType.eqnVariable)
+                .Where(x => x.Template.Type != TQDB_Parser.VariableType.include)
+                // in a perfect world I'd enable that but a lot of the templates have errors :(
+                //.Where(x => x.IsValid())
+                // ignore empty values, maybe whitespace as well?
+                .Where(x => !string.IsNullOrEmpty(x.Value))
+                .ToList();
+            // ArtManager orders this way
+            entries = entries.OrderBy(x => x.Name[0]).ThenBy(x => x.Name).ToList();
+
+            foreach (var entry in entries)
+            {
+                // name of the variable as int index in string table
+                var stringID = AddStrGetIndexLocked(entry.Name, false);
+
+                string[] arraySplit = new string[] { entry.Value };
+                bool isArray = entry.Template.Class == TQDB_Parser.VariableClass.array;
+                if (isArray)
+                    arraySplit = entry.Value.Split(';');
+                short numElements = (short)arraySplit.Length;
+
+                short typeID = -1;
+                object? values = null;
+                bool error = false;
+                // type as int16
+                switch (entry.Template.Type)
+                {
+                    case TQDB_Parser.VariableType.@int:
+                        if (!TryGetIntValues(arraySplit, out values))
+                            error = true;
+                        typeID = 0;
+                        break;
+                    case TQDB_Parser.VariableType.real:
+                        typeID = 1;
+                        if (!TryGetFloatValues(arraySplit, out values))
+                            error = true;
+                        break;
+                    case TQDB_Parser.VariableType.file:
+                        typeID = 2;
+                        values = AddStrsGetIndicesAsync(arraySplit, true);
+                        break;
+                    case TQDB_Parser.VariableType.@string:
+                    case TQDB_Parser.VariableType.equation:
+                        typeID = 2;
+                        values = AddStrsGetIndicesAsync(arraySplit, false);
+                        break;
+                    case TQDB_Parser.VariableType.@bool:
+                        typeID = 3;
+                        if (!TryGetIntValues(arraySplit, out values))
+                            error = true;
+                        break;
+                }
+                if (error || values is null)
+                {
+                    //combinedStrings.Remove(entry.Name);
+                    continue;
+                }
+
+                WriteVariable(fileVarsStream, stringID, typeID, values);
+            }
+            // compress the values using ZLib
+            var myBinaryValuesStream = CompressValues(fileVarsStream);
+
+            // dbr file info
+            DBRFileInfo fileInfo = CreateFileInfo(file, fileNameID, (int)myBinaryValuesStream.Length);
+
+            addToList.Invoke((fileInfo, myBinaryValuesStream));
+
+            FileDone?.Invoke(file.FilePath);
         }
 
         private static DBRFileInfo CreateFileInfo(DBRFile file, int nameID, int length)
@@ -306,7 +318,7 @@ namespace TQArchive_Wrapper
             lock (stringDictionaryLock)
             {
                 for (int i = 0; i < strs.Length; i++)
-                    ret[i] = AddStrGetIndex(strs[i], ignoreCase);
+                    ret[i] = AddStrGetIndex(strs[i], false);
             }
             return ret;
         }
